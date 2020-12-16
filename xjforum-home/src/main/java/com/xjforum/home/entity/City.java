a8b0cf2061cd9df4b1364741cf7ae975b40478aa
admin <597647589@qq.com>
2020-12-16 18:27:51 +0800

    xjforum多模块

--- xjforum-home/src/main/java/com/xjforum/home/entity/City.java
+++ xjforum-home/src/main/java/com/xjforum/home/entity/City.java
@@ -7,7 +7,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.io.Serializable;
 
-
+@Getter
+@Setter
 @Entity
 @Table(name = "city")
 public class City implements Serializable {
@@ -15,29 +16,21 @@ public class City implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
-    @Getter
-    @Setter
+
     private int ID;
     @Column(name = "Name")
     @ApiModelProperty(value = "城市名字", dataType = "String", name = "name", example = "Kabul")
-    @Getter
-    @Setter
+
     private String Name;
 
     @Column(name = "Countrycode")
-    @Getter
-    @Setter
     @ApiModelProperty(value = "城市编码", dataType = "String", name = "Countrycode", example = "AFG")
     private String CountryCode;
 
     @Column(name = "District")
-    @Getter
-    @Setter
     @ApiModelProperty(value = "省", dataType = "String", name = "District", example = "Qandahar")
     private String District;
     @Column(name = "Population")
-    @Getter
-    @Setter
     @ApiModelProperty(value = "城市人口", dataType = "int", name = "Population", example = "1780000")
     private int Population;
 
